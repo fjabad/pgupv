d11bd5624f4144b34600ecccad6bd0d11f811550
Paco Abad <fjabad@dsic.upv.es>
2024-02-21 16:13:25 +0100

    StockProgram nuevo

--- PGUPV/include/stockPrograms.h
+++ PGUPV/include/stockPrograms.h
@@ -8,125 +8,191 @@
 
 namespace PGUPV {
 	//void builder(StockPrograms stockProgramId, Program &p);
-	void buildConstantShading(PGUPV::Program &program);
-	void buildReplaceTexture(PGUPV::Program &program);
-	void buildConstantColorUniform(PGUPV::Program &program);
+	void buildConstantShading(PGUPV::Program& program);
+	void buildReplaceTexture(PGUPV::Program& program);
+	void buildConstantColorUniform(PGUPV::Program& program);
+	void buildConstantColorUniformMVP(PGUPV::Program& program);
+	void buildConstantShadingMVP(Program& program);
 
-
-  template <void (*builder)(Program &)>
-  class StockProgram {
-  public:
-    /**
-    Instala el programa en la GPU
-    */
-    static Program *use() {
-      init();
-      return instance->program.use();
-    }
-    /**
-    \return una referencia al programa.
-    */
-    static Program &getProgram() {
-      init();
-      return instance->program;
-    }
-  protected:
-    // No puedes instanciar objetos de esta clase. 
-    StockProgram() {
-		INFO("StockProgram creado");
+	template <void (*builder)(Program&)>
+	class StockProgram {
+	public:
+		/**
+		Instala el programa en la GPU
+		*/
+		static Program* use() {
+			init();
+			return instance->program.use();
+		}
+		/**
+		\return una referencia al programa.
+		*/
+		static Program& getProgram() {
+			init();
+			return instance->program;
+		}
+	protected:
+		// No puedes instanciar objetos de esta clase. 
+		StockProgram() {
+			INFO("StockProgram creado");
+		};
+		static void init() {
+			if (!instance) {
+				instance = new StockProgram<builder>();
+				builder(instance->program);
+				SetLongevity<StockProgram<builder>>(instance, 5, Private::Deleter<StockProgram<builder>>::Delete);
+			}
+		}
+		static StockProgram<builder>* instance;
+		Program program;
 	};
-    static void init() {
-      if (!instance) {
-        instance = new StockProgram<builder>();
-        builder(instance->program);
-		SetLongevity<StockProgram<builder>>(instance, 5, Private::Deleter<StockProgram<builder>>::Delete);
-      }
-    }
-    static StockProgram<builder> *instance;
-    Program program;
-  };
 
-  template <void(*builder)(Program &)>
-  StockProgram<builder> *StockProgram<builder>::instance;
+	template <void(*builder)(Program&)>
+	StockProgram<builder>* StockProgram<builder>::instance;
 
-  /**
-  \class ConstantIllumProgram
-  Clase que encapsula un programa que dibuja las primitivas interpolando el color
-  de los vértices (las primitivas dibujadas deben definir un color por vértice).
-  No tienes que instanciar objetos, sólo tienes que llamar al método use() para
-  instalar el programa:
+	/**
+	\class ConstantIllumProgram
+	Clase que encapsula un programa que dibuja las primitivas interpolando el color
+	de los vértices (las primitivas dibujadas deben definir un color por vértice).
+	No tienes que instanciar objetos, sólo tienes que llamar al método use() para
+	instalar el programa:
 
-  ConstantIllumProgram::use();
+	ConstantIllumProgram::use();
 
-  model.render();
+	model.render();
 
-  */
+	*/
 
 
-  typedef StockProgram<buildConstantShading> ConstantIllumProgram;
-
-  /**
-  \class TextureReplaceProgram
-  Programa que aplica al polígono dibujado una textura tal cual, según las
-  coordenadas de textura establecidas por el polígono.
-  No tienes que instanciar objetos, sólo tienes que llamar al método use() para
-  instalar el programa. El método setTextureUnit define en qué unidad de textura
-  está la textura a utilizar.
-
-  TextureReplaceProgram::use();
-  TextureReplaceProgram::setTextureUnit(2);
-
-  model.render();
-  */
-  class TextureReplaceProgram {
-  public:
-    /**
-    Instala el programa en la GPU
-    */
-    static Program *use();
-    /**
-    Establece la unidad de textura donde se encuentra la textura a dibujar
-    \param texUnit la unidad de textura a usar (p.e., 0 para GL_TEXTURE0)
-    \return la unidad de textura que se estaba usando
-    \warning El programa debe estar instalado en la GPU (con 'use' *antes* de llamar a este
-    método)
-    \warning ¡Cuidado! Sólo hay una instancia de este programa para toda la aplicación,
-    por lo que si cambias la unidad de textura y usas el programa en varios sitios de
-    tu aplicación, todos ellos usarán la nueva unidad, hasta que haya un nuevo cambio.
-    */
-    static int setTextureUnit(int texUnit);
-  private:
-    static int currentTexUnit;
-  };
+	typedef StockProgram<buildConstantShading> ConstantIllumProgram;
 
 
+	/**
+	\class ConstantIllumProgramMVP
+	Igual que ConstantIllumProgram, pero especificando directamente la matriz de transformación
 
-  /**
-  \class ConstantUniformColorProgram
-  Programa que aplica a la primitiva un color constante especificado en tiempo 
-  de ejecución.
-  No tienes que instanciar objetos, sólo tienes que llamar al método use() para
-  instalar el programa. El método setColor define el color a utilizar.
+	ConstantIllumProgramMVP::use();
 
-  ConstantUniformColorProgram::use();
-  ConstantUniformColorProgram::setColor(glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
+	glm::mat4 mvp;
+	[...]
+	ConstantIllumProgramMVP::setMVP(mvp);
 
-  model.render();
-  */
-  class ConstantUniformColorProgram {
-  public:
-	  /**
-	  Instala el programa en la GPU
-	  */
-	  static Program *use();
-	  /**
-	  Establece el color que se usará para dibujar las primitivas
-	  \param color el color
-	  \warning El programa debe estar instalado en la GPU (con 'use' *antes* de llamar a este
-	  método)
-	  */
-	  static void setColor(const glm::vec4 &color);
-  private:
-	  static GLint colorLoc;
-  };
+	model.render();
+
+	*/
+
+	class ConstantIllumProgramMVP {
+	public:
+		/**
+		Instala el programa en la GPU
+		*/
+		static Program* use();
+		/**
+		Establece la matriz de transformación model-view-projection a usar
+		\warning El programa debe estar instalado en la GPU (con 'use' *antes* de llamar a este
+		método)
+		*/
+		static void setMVP(const glm::mat4& mvp);
+	private:
+		static GLint mvpLoc;
+	};
+
+	/**
+	\class TextureReplaceProgram
+	Programa que aplica al polígono dibujado una textura tal cual, según las
+	coordenadas de textura establecidas por el polígono.
+	No tienes que instanciar objetos, sólo tienes que llamar al método use() para
+	instalar el programa. El método setTextureUnit define en qué unidad de textura
+	está la textura a utilizar.
+
+	TextureReplaceProgram::use();
+	TextureReplaceProgram::setTextureUnit(2);
+
+	model.render();
+	*/
+	class TextureReplaceProgram {
+	public:
+		/**
+		Instala el programa en la GPU
+		*/
+		static Program* use();
+		/**
+		Establece la unidad de textura donde se encuentra la textura a dibujar
+		\param texUnit la unidad de textura a usar (p.e., 0 para GL_TEXTURE0)
+		\return la unidad de textura que se estaba usando
+		\warning El programa debe estar instalado en la GPU (con 'use' *antes* de llamar a este
+		método)
+		\warning ¡Cuidado! Sólo hay una instancia de este programa para toda la aplicación,
+		por lo que si cambias la unidad de textura y usas el programa en varios sitios de
+		tu aplicación, todos ellos usarán la nueva unidad, hasta que haya un nuevo cambio.
+		*/
+		static int setTextureUnit(int texUnit);
+	private:
+		static int currentTexUnit;
+	};
+
+
+
+	/**
+	\class ConstantUniformColorProgram
+	Programa que aplica a la primitiva un color constante especificado en tiempo
+	de ejecución.
+	No tienes que instanciar objetos, sólo tienes que llamar al método use() para
+	instalar el programa. El método setColor define el color a utilizar.
+
+	ConstantUniformColorProgram::use();
+	ConstantUniformColorProgram::setColor(glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
+
+	model.render();
+	*/
+	class ConstantUniformColorProgram {
+	public:
+		/**
+		Instala el programa en la GPU
+		*/
+		static Program* use();
+		/**
+		Establece el color que se usará para dibujar las primitivas
+		\param color el color
+		\warning El programa debe estar instalado en la GPU (con 'use' *antes* de llamar a este
+		método)
+		*/
+		static void setColor(const glm::vec4& color);
+	private:
+		static GLint colorLoc;
+	};
+
+	/**
+	\class ConstantUniformColorProgramMVP
+	Igual que ConstantUniformColorProgram, pero especificando directamente la matriz de
+	transformación, en vez de GLMatrices.
+
+	glm::mat4 mvp;
+	[...]
+	ConstantUniformColorProgramMVP::use();
+	ConstantUniformColorProgramMVP::setMVP(mvp);
+	ConstantUniformColorProgram::setColor(glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
+
+	model.render();
+	*/
+	class ConstantUniformColorProgramMVP {
+	public:
+		/**
+		Instala el programa en la GPU
+		*/
+		static Program* use();
+		/**
+		Establece el color que se usará para dibujar las primitivas
+		\param color el color
+		\warning El programa debe estar instalado en la GPU (con 'use' *antes* de llamar a este
+		método)
+		*/
+		static void setColor(const glm::vec4& color);
+		/**
+		Establece la matriz de transformación model-view-projection a usar
+		*/
+		static void setMVP(const glm::mat4& mvp);
+	private:
+		static GLint colorLoc, mvpLoc;
+	};
 };
--- PGUPV/src/stockPrograms.cpp
+++ PGUPV/src/stockPrograms.cpp
@@ -32,6 +32,32 @@ void PGUPV::buildConstantShading(Program &program) {
 	program.loadStrings(vtxShaderSrc, frgShaderSrc);
 }
 
+
+void PGUPV::buildConstantShadingMVP(Program &program) {
+	program.addAttributeLocation(Mesh::VERTICES, "position");
+	program.addAttributeLocation(Mesh::COLORS, "vertcolor");
+	std::vector<std::string> vtxShaderSrc{
+	  "#version 420",
+	  "uniform mat4 modelviewprojMatrix;",
+	  "in vec4 position;",
+	  "in vec4 vertcolor;",
+	  "out vec4 fragcolor;",
+	  "void main() {",
+	  "  fragcolor = vertcolor;",
+	  "  gl_Position = modelviewprojMatrix * position;",
+	  "}"
+	};
+	std::vector<std::string> frgShaderSrc{
+	  "#version 420",
+	  "in vec4 fragcolor;",
+	  "out vec4 final_color;",
+	  "void main() {",
+	  "  final_color = fragcolor;",
+	  "}"
+	};
+	program.loadStrings(vtxShaderSrc, frgShaderSrc);
+}
+
 int TextureReplaceProgram::currentTexUnit;
 
 int TextureReplaceProgram::setTextureUnit(int texUnit) {
@@ -108,4 +134,61 @@ void PGUPV::buildConstantColorUniform(Program &program) {
 		"}"
 	};
 	program.loadStrings(vtxShaderSrc, frgShaderSrc);
+}
+
+GLint ConstantUniformColorProgramMVP::colorLoc = -1;
+GLint ConstantUniformColorProgramMVP::mvpLoc= -1;
+
+Program *ConstantUniformColorProgramMVP::use() {
+	return StockProgram<PGUPV::buildConstantColorUniformMVP>::use();
+}
+
+
+void ConstantUniformColorProgramMVP::setColor(const glm::vec4 &color) {
+	if (colorLoc == -1) 
+		colorLoc = StockProgram<PGUPV::buildConstantColorUniformMVP>::getProgram().getUniformLocation("color");
+	glUniform4fv(colorLoc, 1, &color.x);
+}
+
+void ConstantUniformColorProgramMVP::setMVP(const glm::mat4& mvp)
+{
+	if (mvpLoc == -1)
+		mvpLoc = StockProgram<PGUPV::buildConstantColorUniformMVP>::getProgram().getUniformLocation("modelviewprojMatrix");
+	glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, &mvp[0][0]);
+}
+
+
+void PGUPV::buildConstantColorUniformMVP(Program &program) {
+	program.addAttributeLocation(Mesh::VERTICES, "position");
+	std::vector<std::string> vtxShaderSrc{
+		"#version 420",
+		"uniform mat4 modelviewprojMatrix;",
+		"in vec4 position;",
+		"void main() {",
+		"  gl_Position = modelviewprojMatrix * position;",
+		"}"
+	};
+	std::vector<std::string> frgShaderSrc{
+		"#version 420",
+		"uniform vec4 color;",
+		"out vec4 final_color;",
+		"void main() {",
+		"  final_color = color;",
+		"}"
+	};
+	program.loadStrings(vtxShaderSrc, frgShaderSrc);
+}
+
+GLint ConstantIllumProgramMVP::mvpLoc = -1;
+
+Program* ConstantIllumProgramMVP::use()
+{
+	return StockProgram<PGUPV::buildConstantShadingMVP>::use();
+}
+
+void ConstantIllumProgramMVP::setMVP(const glm::mat4& mvp)
+{
+	if (mvpLoc == -1)
+		mvpLoc = StockProgram<PGUPV::buildConstantShadingMVP>::getProgram().getUniformLocation("modelviewprojMatrix");
+	glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, &mvp[0][0]);
 }
