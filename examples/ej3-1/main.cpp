0e96798e5f39848f781c1ec339f355e6f83ea1e0
Paco Abad <fjabad@dsic.upv.es>
2024-03-11 12:55:54 +0100

    Definiendo la malla desde cero

--- examples/ej3-1/main.cpp
+++ examples/ej3-1/main.cpp
@@ -23,7 +23,7 @@ public:
 private:
 	std::shared_ptr<GLMatrices> mats;
 	std::shared_ptr<Texture2D> texture;
-	Rect plane;
+	Model plane;
 };
 
 void MyRender::setup() {
@@ -32,16 +32,23 @@ void MyRender::setup() {
 
 	mats = GLMatrices::build();
 
-	// Cambiando las coordenadas del objeto Rect original. Por defecto,
-	// las coordenadas de textura de dicho objeto están entre 0 y 1 (mira
-	// el constructor de Rect en stockModels.cpp
-	glm::vec2 tc[] = {
-	  glm::vec2(0.0f, 0.0f), glm::vec2(2.0f, 0.0f),
-	  glm::vec2(2.0f, 2.0f), glm::vec2(0.0f, 2.0f) };
+	// Creamos un modelo que representa un plano de 1x1
+	const std::vector<glm::vec3> vertices{
+		{-0.5f, -0.5f, 0.0f},
+		{0.5f, -0.5f, 0.0f},
+		{0.5f, 0.5f, 0.0f},
+		{-0.5f, 0.5f, 0.0f } };
+	// Coordenadas de textura para el plano
+	const std::vector<glm::vec2> tc{
+		{0.0f, 0.0f}, {2.0f, 0.0f},
+		{2.0f, 2.0f}, {0.0f, 2.0f} };
 
-	// Añadir las coordenadas de textura a la primera malla (y única)
-	// malla del plano.
-	plane.getMesh(0).addTexCoord(0, tc, 4);
+	auto m = std::make_shared<Mesh>();
+	m->addVertices(vertices);
+	m->addTexCoord(0, tc);
+	m->addDrawCommand(new PGUPV::DrawArrays(GL_TRIANGLE_FAN, 0, static_cast<GLsizei>(vertices.size())));
+
+	plane.addMesh(m);
 
 	// Cargamos la nueva textura desde un fichero
 	texture = std::make_shared<Texture2D>();
@@ -72,8 +79,8 @@ void MyRender::reshape(uint w, uint h) {
 	mats->setMatrix(GLMatrices::PROJ_MATRIX, getCamera().getProjMatrix());
 }
 
-int main(int argc, char *argv[]) {
-	App &myApp = App::getInstance();
+int main(int argc, char* argv[]) {
+	App& myApp = App::getInstance();
 	myApp.initApp(argc, argv, PGUPV::DOUBLE_BUFFER | PGUPV::DEPTH_BUFFER |
 		PGUPV::MULTISAMPLE);
 	myApp.getWindow().setRenderer(std::make_shared<MyRender>());
