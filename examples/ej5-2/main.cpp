1062ea16bd0c63b7c13aa98d2bb7046f3f9ac3a8
Paco Abad <fjabad@dsic.upv.es>
2026-03-23 19:16:59 +0100

    Error en la ruta del shader

--- examples/ej5-2/main.cpp
+++ examples/ej5-2/main.cpp
@@ -73,7 +73,7 @@ void MyRender::setup() {
   dshader.addAttributeLocation(Mesh::VERTICES, "position");
   dshader.addAttributeLocation(Mesh::COLORS, "vertcolor");
 
-  dshader.loadFiles(App::examplesDir() + "ej5-3/ubo-dummy");
+  dshader.loadFiles(App::examplesDir() + "ej5-2/ubo-dummy");
   dshader.compile();
 
   // 1. Calcular el índice del bloque
